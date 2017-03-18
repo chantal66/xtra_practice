7c5c7e61a9d88884dcdb8135f4c72da67561d37e
Chantal Justamond <cjustamm66@me.com>
2017-03-18 17:20:09 -0600

    string the hard way

---
+++ strings_the_hard_way.rb
@@ -0,0 +1,9 @@
+p 'pizza'.length
+p 'pizza'.upcase
+p 'dog'.sub('d','f')
+p 'dud'.sub('d','f') #changes only the first parameter that finds.
+puts 'dud'.gsub('d','f') #changes both parameters found
+p 'Hello Dear Friends'.split
+p 'one,two,three'.split(',')
+p 'pizza'[3, 4]
+p 'pizza'[1..4]
